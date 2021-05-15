0ccd796e0eba435640a24aefb0f45da719d71ee4
ArtARTs36 <temicska99@mail.ru>
2021-05-15 23:12:13 +0300

    remove helpers

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -21,21 +21,6 @@ function post()
     return method(Post::class, $_POST ?? []);
 }
 
-function get()
-{
-    return method(\App\Http\Get::class, $_GET ?? []);
-}
-
-function uri(): ?string
-{
-    return $_SERVER['REQUEST_URI'] ?? null;
-}
-
-function is_current_uri(string $search): bool
-{
-    return strpos(uri(), $search) === 1;
-}
-
 function view(string $template, array $attributes = []): string
 {
     return Viewer::render($template, $attributes);
@@ -44,17 +29,6 @@ function view(string $template, array $attributes = []): string
 function view_path(string $template): string
 {
     return __DIR__ . '/../../views/' . $template . '.tpl';
-}
-
-function cache(string $key, \Closure $default, int $ttl = 24 * 3600)
-{
-    static $instance = null;
-
-    if (null === $instance) {
-        $instance = new Cache(__DIR__  . '/../../var/cache');
-    }
-
-    return $instance->remember($key, $default, $ttl);
 }
 
 function dd(...$vars)
