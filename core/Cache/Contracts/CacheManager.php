56b521e698d7ee3b1da8d1ebe35585bb0cf99625
ArtARTs36 <temicska99@mail.ru>
2021-05-16 11:53:29 +0300

    update cache manager

--- core/Cache/Contracts/CacheManager.php
+++ core/Cache/Contracts/CacheManager.php
@@ -4,5 +4,7 @@ namespace Core\Cache\Contracts;
 
 interface CacheManager
 {
+    public function forgetAll(): void;
 
+    public function remember(string $key, \Closure $callback, int $ttl = 3600 * 24);
 }
