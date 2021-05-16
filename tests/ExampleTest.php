d1ebf147d562db6ff6a8d3a0b2397e54da262b7e
ArtARTs36 <temicska99@mail.ru>
2021-05-16 10:53:35 +0300

    add example test

---
+++ tests/ExampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests;
+
+class ExampleTest extends TestCase
+{
+    public function testBasicTest(): void
+    {
+        $this->assertTrue(true);
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use Core\DependencyInjection\Container;
+use Core\DependencyInjection\ContainerBuilder;
 use Core\Exception\Contracts\ExceptionHandler;
 use core\Exception\NullExceptionHandler;
 
@@ -20,6 +21,8 @@ abstract class TestCase extends \PHPUnit\Framework\TestCase
 
     protected function install(): void
     {
+        $this->appContainer = (new ContainerBuilder())->build();
+
         $this
             ->appContainer
             ->contract(ExceptionHandler::class, NullExceptionHandler::class);
